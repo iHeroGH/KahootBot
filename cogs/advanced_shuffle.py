8e8c6b90a9d5c997fbbc6b544e7911bfd46a8240
George Matta <jojomedhat2004@gmail.com>
2021-11-17 00:27:56 -0800

    no duplicates

--- cogs/advanced_shuffle.py
+++ cogs/advanced_shuffle.py
@@ -22,7 +22,10 @@ class AdvancedShuffle(vbu.Cog):
                 curr_id = curr_split[0]
                 curr_name = " ".join(curr_split[1:])
 
-                await db("INSERT INTO name_id_pairs (channel_id, name, id) VALUES ($1, $2, $3)", ctx.channel.id, curr_name, curr_id)
+                try:
+                    await db("INSERT INTO name_id_pairs (channel_id, name, id) VALUES ($1, $2, $3)", ctx.channel.id, curr_name, curr_id)
+                except:
+                    await ctx.send(f"The name {curr_name} or the ID {curr_id} is already in use, so I skipped it!")
 
             curr_pairs = await db("SELECT name, id FROM name_id_pairs WHERE channel_id = $1", ctx.channel.id)
 
