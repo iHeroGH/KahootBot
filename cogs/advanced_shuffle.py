2a23eccd5ef47b59c33a839d5a759468cac39f07
George Matta <jojomedhat2004@gmail.com>
2021-11-17 00:55:09 -0800

    better format how we take pairs in

--- cogs/advanced_shuffle.py
+++ cogs/advanced_shuffle.py
@@ -9,28 +9,28 @@ class AdvancedShuffle(vbu.Cog):
         A (quite advanced) command to add multiple name-ID pairs to a channel at once
 
         Format:
-        /add id name, id2 name2, id3 name3
+        /add name: id, name2: id2, name3: id3
 
-        Note: Names can't have a comma in them
+        Note: Names can't have a colon in them
         """
 
         pairs = pairs.split(",")
 
         async with self.bot.database() as db:
+            added = False
             for pair in pairs:
-                curr_split = pair.strip().split(" ")
-                curr_id = curr_split[0]
-                curr_name = " ".join(curr_split[1:])
+                curr_split = pair.split(":")
+                curr_name = curr_split[0].strip
+                curr_id = curr_split[1].strip()
 
                 try:
                     await db("INSERT INTO name_id_pairs (channel_id, name, id) VALUES ($1, $2, $3)", ctx.channel.id, curr_name, curr_id)
+                    added = True
                 except:
-                    await ctx.send(f"The pair {curr_name} - {curr_id} had one or more repeating values in the database!")
+                    await ctx.send(f"The pair {curr_name}: {curr_id} had one or more repeating values in the database!")
 
-        curr_pairs = await self.get_from_db(ctx)
-        pairs_message = "Interpreted list:\n" + self.get_formatted_message(curr_pairs)
-
-        await ctx.send(pairs_message)
+        if added:
+            await ctx.send("Added the new values! Check the list by running the `list` command")
 
     @vbu.command(aliases=['getid', 'getids', "listids", "listid"])
     async def list(self, ctx: vbu.Context):
