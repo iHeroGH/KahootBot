41a3fc966b52ea037d402b88533529cde4f2cd93
George Matta <jojomedhat2004@gmail.com>
2021-11-17 01:35:36 -0800

    we literally get a name from an id what's wrong with me

--- cogs/advanced_shuffle.py
+++ cogs/advanced_shuffle.py
@@ -6,33 +6,31 @@ import cogs.localutils.helper_functions as utils
 class AdvancedShuffle(vbu.Cog):
 
     @vbu.command(aliases=['addids'])
-    async def add(self, ctx: vbu.Context, *, pairs: str):
+    async def add(self, ctx: vbu.Context, *, ids: str):
         """
-        A (quite advanced) command to add multiple name-ID pairs to a channel at once
+        A (quite advanced) command to add multiple IDs to a channel at once
 
         Format:
-        /add name: id, name2: id2, name3: id3
+        /add ID, ID2, ID3, ID4
 
-        Note: Names can't have a colon in them
         """
 
-        pairs = pairs.split(",")
+        ids = ids.split(",")
 
         async with self.bot.database() as db:
             added = False
-            for pair in pairs:
-                curr_split = pair.split(":")
-                curr_name = curr_split[0].strip()
-                curr_id = curr_split[1].strip()
+            for id in ids:
 
-                if not (await utils.validate_requester(ctx, curr_id))[1]: # If it ain't valid
+                if not (_, req := await utils.validate_requester(ctx, id))[1]: # If it ain't valid
                     continue
 
+                curr_name = req.get_title()
+
                 try:
-                    await db("INSERT INTO name_id_pairs (channel_id, name, id) VALUES ($1, $2, $3)", ctx.channel.id, curr_name, curr_id)
+                    await db("INSERT INTO name_id_pairs (channel_id, name, id) VALUES ($1, $2, $3)", ctx.channel.id, curr_name, id)
                     added = True
                 except:
-                    await ctx.send(f"The pair {curr_name}: {curr_id} had one or more repeating values in the database!")
+                    await ctx.send(f"The pair {curr_name}: {id} had one or more repeating values in the database!")
 
         if added:
             await ctx.send("Added the new values! Check the list by running the `list` command")
