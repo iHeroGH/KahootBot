3f6dcf71c0787c2707e52777aaa02b050fc0c82c
George Matta <jojomedhat2004@gmail.com>
2021-11-16 23:29:37 -0800

    await game creation

--- cogs/kahoot.py
+++ cogs/kahoot.py
@@ -78,7 +78,7 @@ class KahootCommand(vbu.Cog):
         Plays a quiz
         """
         # Create a game and see if we succeeded
-        kahoot_game = KahootGame.create_game(ctx, kahoot)
+        kahoot_game = await KahootGame.create_game(ctx, kahoot)
         if not isinstance(kahoot_game, KahootGame):
             return
 
