3824f28da8918eae6637a9b70c8ef11adbab412e
George Matta <jojomedhat2004@gmail.com>
2021-11-17 00:40:51 -0800

    send the final list after making it

--- cogs/advanced_shuffle.py
+++ cogs/advanced_shuffle.py
@@ -25,14 +25,21 @@ class AdvancedShuffle(vbu.Cog):
                 try:
                     await db("INSERT INTO name_id_pairs (channel_id, name, id) VALUES ($1, $2, $3)", ctx.channel.id, curr_name, curr_id)
                 except:
-                    await ctx.send(f"The name {curr_name} or the ID {curr_id} is already in use, so I skipped it!")
+                    await ctx.send(f"The pair {curr_name} - {curr_id} had one or more repeating values in the database!")
 
             curr_pairs = await db("SELECT name, id FROM name_id_pairs WHERE channel_id = $1", ctx.channel.id)
 
+        pairs_message = "Interpreted list:\n" + self.get_formatted_message(curr_pairs)
 
-        await ctx.send(curr_pairs)
+        await ctx.send(pairs_message)
 
+    def get_formatted_message(pairs):
+        final_message = "__**Name: ID**__"
 
+        for pair in pairs:
+            final_message += f"\n{pair['name']}: {pair['id']}"
+
+        return final_message
 
 def setup(bot: vbu.Bot):
     x = AdvancedShuffle(bot)
