d1347c22f73495da27ac3fb73a905c30d86374c3
GEORGE MATTA <jojomedhat2004@gmail.com>
2023-05-23 23:13:25 -0700

    remove aliases

--- cogs/kahoot.py
+++ cogs/kahoot.py
@@ -9,8 +9,7 @@ class KahootCommand(vbu.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(aliases=['kahootdata', 'getdata', 'get'], 
-                      application_command_meta=commands.ApplicationCommandMeta(
+    @commands.command(application_command_meta=commands.ApplicationCommandMeta(
                         options = [
                             discord.ApplicationCommandOption(
                                 name="Kahoot ID",
@@ -67,7 +66,7 @@ class KahootCommand(vbu.Cog):
 
         self.bot.logger.info(f"Data Sent for {kahoot}")
 
-    @commands.command(aliases=['cancelgame', 'end'], application_command_meta=commands.ApplicationCommandMeta(
+    @commands.command(application_command_meta=commands.ApplicationCommandMeta(
                         options = [
                             discord.ApplicationCommandOption(
                                 name="Password",
@@ -93,7 +92,7 @@ class KahootCommand(vbu.Cog):
         await ctx.send("Cancelling the game.")
         KahootGame.remove_session(ctx.channel.id)
 
-    @commands.command(aliases=['kahoot', 'quiz'], application_command_meta=commands.ApplicationCommandMeta(
+    @commands.command(application_command_meta=commands.ApplicationCommandMeta(
                         options = [
                             discord.ApplicationCommandOption(
                                 name="Kahoot ID",
@@ -101,8 +100,9 @@ class KahootCommand(vbu.Cog):
                                 description="The ID of the quiz you want to play",
                                 required=False,
                             )
-                        ]
-                      ))
+                            ]
+                        )
+                      )
     async def play(self, ctx: vbu.Context, kahoot: str = None):
         """
         Plays a quiz
